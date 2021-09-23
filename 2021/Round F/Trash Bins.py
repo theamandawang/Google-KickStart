395254cf808d3971499e51ecbd8362f9e4ae583e
theamandawang <52474776+theamandawang@users.noreply.github.com>
2021-09-23 08:48:31 -0700

    Update Trash Bins.py

--- 2021/Round F/Trash Bins.py
+++ 2021/Round F/Trash Bins.py
@@ -1,5 +1,22 @@
 ###TLE Error
 def bins(can):
+    #we can take a "math" approach to this problem
+    #all the inputs for 'can' will be 1000011100011000 or some similar pattern
+    #we have two types of runs, runs of 0s and runs of 1s.
+    #distance for a house with a bin in front = 0 (disregard this in calculations)
+    #only consider runs of 0s
+    #if the run is odd, (for instance: 1000001) the distance for the 
+    #0 houses, (left to right) are 1, 2, 3, 2, 1. or, 2*SUM(1 to runLength//2)+runLength//2+1
+    
+    #if the run is even, (for instance: 10000001) the distance for the 0 houses (left to right) are 1, 2, 3, 3, 2, 1
+    #or 2*SUM(1 to runLength//2)
+    
+    #"edge" cases, where the 0 run is at the head or tail
+    #take 00001 or 10000
+    #then the distances are 1, 2, 3, 4 (for both), or SUM(1 to runLength)
+    
+    #I used the summation formula from math SUM(1 to n) = n(n+1)/2
+    
     dis = 0
     run = 0
     for c in range(len(can)):
